c2b399826a831d9f781a622ee965c6c6226e0de1
tunglt.netsgame <tunglt@netsgame.vn>
2022-12-08 10:40:14 +0700

    Updated build script.

--- Assets/Scripts/Editor/BuildScript.cs
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEditor;
+using UnityEngine;
 
 public class BuildScript
 {
@@ -18,9 +20,28 @@ public class BuildScript
     public static void BuildAndroid()
     {
         var scenes = EditorBuildSettings.scenes;
+
+        string buildPath = string.Empty;
+        var args = System.Environment.GetCommandLineArgs();
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i].Equals("-buildPath"))
+            {
+                buildPath = args[i + 1];
+                break;
+            }
+        }
+
+        string name = Application.productName;
+        string version = Application.version;
+        string datetime = DateTime.Now.ToString("ddMM.HHmm");
+        string extension = ".apk";
+
+        buildPath += $"{name}_{version}_{datetime}{extension}";
+
         BuildPipeline.BuildPlayer(
             scenes,
-            "C:\\Users\\Admin\\Downloads\\Builds\\CMDBuilds\\Android\\Build.apk",
+            buildPath,
             BuildTarget.Android,
             BuildOptions.None
             );
